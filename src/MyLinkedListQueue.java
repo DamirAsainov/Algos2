793c6937e3a34f6728c77da11c4249c764d3f16d
Damir <dense.neer@gmail.com>
2023-05-01 22:50:43 +0600

    comments for code

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -10,23 +10,45 @@ public class MyLinkedListQueue<T> {
     public void enqueue(Object item){
         list.add(item);
     }
+    /*
+     Inserts the specified element at the end of this queue.
+     @param item the element to add
+     */
     public Object dequeue(){
         if (isEmpty()) {
             throw new NoSuchElementException();
         }
         return list.removeFirst();
     }
+    /*
+     Retrieves and removes the head of this queue.
+     @return the head of this queue
+     @throws NoSuchElementException if this queue is empty
+     */
     public Object peek(){
         if (isEmpty()) {
             throw new NoSuchElementException();
         }
         return list.get(0);
     }
+    /*
+     Retrieves, but does not remove, the head of this queue,
+     or returns null if this queue is empty.
+     @return the head of this queue, or null if this queue is empty
+     */
     public boolean isEmpty(){
         return size() == 0;
     }
+    /*
+     Returns true if this queue contains no elements.
+     @return true if this queue contains no elements, false otherwise
+     */
     public int size(){
         return list.getSize();
     }
+    /*
+     Returns the number of elements in this queue.
+     @return the number of elements in this queue
+     */
 
 }
--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -10,13 +10,21 @@ public class MyLinkedListStack<T> {
     public void push(T item) {
         list.addFirst(item);
     }
-
+    /*
+     Adds an element to the top of the stack.
+     @param item the element to add
+     */
     public Object pop() {
         if (isEmpty()) {
             throw new EmptyStackException();
         }
         return list.removeFirst();
     }
+    /*
+     Removes and returns the top element of the stack.
+     @return the top element of the stack
+     @throws EmptyStackException if the stack is empty
+     */
 
     public Object peek() {
         if (isEmpty()) {
@@ -24,12 +32,25 @@ public class MyLinkedListStack<T> {
         }
         return list.get(0);
     }
+    /*
+     Returns the top element of the stack without removing it.
+     @return the top element of the stack
+     @throws EmptyStackException if the stack is empty
+     */
 
     public boolean isEmpty() {
         return size() == 0;
     }
+    /*
+     Returns true if the stack is empty or false if it is not.
+     @return true if the stack is empty or false if it is not
+     */
 
     public int size() {
         return list.getSize();
     }
+    /*
+     Returns the number of elements in the stack.
+     @return the number of elements in the stack
+     */
 }
